28c9825357b6de0568007a872593619350160d4c
YuriySamusew <6177385@gmail.com>
2022-10-02 22:10:03 +0300

    3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const NOTIFY_TIMEOUT = 5000; //ms
+
+const promisesFormRef = document.querySelector('.form');
+
+promisesFormRef.addEventListener('submit', onPromisesFormSubmit);
+
+function onPromisesFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const formDataObj = {};
+  formData.forEach((value, key) => {
+    formDataObj[key] = Number(value);
+  });
+  // console.log(formDataObj);
+  createPromisesArray(formDataObj);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promice((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`Fulfilled promice ${position + 1} in ${delay} ms`);
+      } else {
+        // Reject
+        reject(`Rejected promice ${position + 1} in ${delay} ms`);
+      }
+    }, delay);
+  })
+    .then(value => Notify.success(value, { timeout: NOTIFY_TIMEOUT }))
+    .catch(error => Notify.failure(error, { timeout: NOTIFY_TIMEOUT }));
+}
+
+function onPromisesFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const formDataObj = {};
+  formData.forEach((value, key) => {
+    formDataObj[key] = Number(value);
+  });
+  // console.log(formDataObj);
+  createPromisesArray(formDataObj);
+}
+
+function createPromisesArray({ delay, step, amount }) {
+  const delaysArray = Array.from(
+    { length: amount },
+    (_, i) => delay + i * step
+  );
+  // console.log(delaysArray);
+  const promises = delaysArray.map(createPromise);
+  console.log(promises);
 }
